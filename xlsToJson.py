b7eecce133d81b7b8c92a4b4660263addfad7d4d
Imri <imri.luzzattov@gmail.com>
2020-02-27 23:55:57 +0200

    Connect to data base

--- xlsToJson.py
+++ xlsToJson.py
@@ -1,11 +1,19 @@
-import xlrd 
-from xlrd.sheet import ctype_text 
-from datetime import datetime
+import argparse
 import json
+import xlrd
 
-def main():
-	print("start working on the project")
-	convertToJson("new_one.xls")
+from datetime import datetime
+from db_handler import connect_to_db
+from db_handler import insert_db
+from pymongo import MongoClient
+from xlrd.sheet import ctype_text
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='Process some integers.')
+	parser.add_argument('--sheet_name', help='sum the integers (default: find the max)', default="new_one.xls", required=False)
+
+	return parser.parse_args()
 
 
 def isXldateType(cell):
@@ -13,44 +21,36 @@ def isXldateType(cell):
 		return True
 	return False
 
+
 def changeXldateToDatatime(cell,workbook):
 	return xlrd.xldate.xldate_as_datetime(cell.value, workbook.datemode)
 
-def convertToJson(filename):
-	curr_workbook = xlrd.open_workbook(filename)
 
-	sheet_names = curr_workbook.sheet_names()
+def create_data(sheet, curr_workbook):
+	transactions = list()
+	for i in range(sheet.nrows):
+		row = sheet.row(i)
+		#unpack row to vars ,str each element for json 
+		[date,bussines_name,deal_value,chrage_value,another_deatils] = [str(element).replace("text",'') for element in row]
+		if(isXldateType(row[0])):
+			date = str(changeXldateToDatatime(row[0], curr_workbook))
+		curr_deal = {'deal_date ' : date ,'bussines_name' : (bussines_name)  ,'deal_value' : (deal_value),'chrage_value' : (chrage_value),
+		'more deatils' : (another_deatils)}
+		transactions.append(curr_deal)
+	return transactions
 
-	first_sheet = curr_workbook.sheet_by_name(sheet_names[0])
 
+def main():
+	args = parse_args()
+	curr_workbook = xlrd.open_workbook(args.sheet_name)
 	first_sheet = curr_workbook.sheet_by_index(0)
 	print ('Sheet name: %s' % first_sheet.name)
 
-	with open('jsonFromXls.txt','w') as json_file:
-		deals = []
-		for i in range(first_sheet.nrows):
-			row = first_sheet.row(i)
-			#unpack row to vars ,str each element for json 
-			[date,bussines_name,deal_value,chrage_value,another_deatils] = [str(element).replace("text",'') for element in row]
-			if(isXldateType(row[0])):
-				date = str(changeXldateToDatatime(row[0], curr_workbook))
-			curr_deal = {'deal_date ' : date ,'bussines_name' : (bussines_name)  ,'deal_value' : (deal_value),'chrage_value' : (chrage_value),
-			'more deatils' : (another_deatils)}
-			deals.append(curr_deal)
-		#ensure ascii = False for current hebrew output
-		json.dump(deals,json_file,ensure_ascii = False,indent = 4)
-	json_file.close()
+	data_arr = create_data(first_sheet, curr_workbook)
 
-
-
-
-
-
-
-
-
-
-
+	db_con = connect_to_db("mongodb+srv://imridb:imri123!@creditdata-xurnm.mongodb.net/test", "test-collection")
+	print("Connected!")
+	insert_db(db_con, data_arr)
 
 
 if __name__ == "__main__":
